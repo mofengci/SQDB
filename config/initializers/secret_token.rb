a28106e6cdfd1347d673146bd5a71ebde67652a1
mofengci <mofengci@gmail.com>
2012-12-13 11:03:23 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SQDB::Application.config.secret_token = '810b191a8dc3f57427c044764e37bd204ada2f1944491befa3f97c6306276a0ee862c1c716b1ddcca72f8aba7e43d291b976d83733a1ec49f11514882c5abb0d'
