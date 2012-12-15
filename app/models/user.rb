863afc9cab71effc569b58e16aa67c714d8fb259
mofengci <mofengci@gmail.com>
2012-12-16 05:42:41 +0800

    finshed user edit update,nearly finished index,none destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -25,7 +26,7 @@ class User < ActiveRecord::Base
 
   private
 
-  def create_remember_token
-    self.remember_token= SecureRandom.urlsafe_base64
-  end
+    def create_remember_token
+      self.remember_token= SecureRandom.urlsafe_base64
+    end
 end
