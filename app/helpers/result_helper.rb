09742b84b1f50b29a01dcadf445119f8f858ae64
mofengci <mofengci@gmail.com>
2012-12-27 15:13:13 +0800

    improved the help page

--- app/helpers/result_helper.rb
+++ app/helpers/result_helper.rb
@@ -1,8 +1,29 @@
 module ResultHelper
 
   def find_user (id)
-    user=User.find(id)
-    user.name
+    @user=User.find(id)
+    @user.name
   end
+	
+	def render_result
+		@users=User.all
+		@results=Result.all
+		#清空所有排班信息
+		(0..14).to_a.each do |i|
+			@result=Result.find(i)
+			@result.manager=0
+			@result.coach=0
+			@result.female=0
+			@result.male1=0
+			@result.male2=0
+			@result.save
+		end
+		#重新生成排班信息
+		(0..14).to_a.each do |i|
+			@users.each do |person|
+				
+			end
+		end
+	end
 
 end
